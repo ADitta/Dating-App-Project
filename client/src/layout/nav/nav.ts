2d65d1a4276f83180c8c4c61d8fd7aeecfae56ae
ADitta <ADitta@users.noreply.github.com>
2025-12-13 18:45:13 +0000

    add basic register functionality

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
+  protected accountService = inject(AccountService);
+  protected creds: any = {};
 
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.creds = {};
+      },
+      error: (err) => {
+        alert(err.message);
+      },
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
