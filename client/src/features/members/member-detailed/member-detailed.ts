e60817cd9fc56bc2ebf715107a94667f1da16375
ADitta <ADitta@users.noreply.github.com>
2025-12-22 13:32:44 +0000

    add edit functionality and simple caching

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import {
   ActivatedRoute,
@@ -12,6 +12,7 @@ import { AsyncPipe } from '@angular/common';
 import { filter, Observable } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-member-detailed',
@@ -20,16 +21,17 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
   styleUrl: './member-detailed.css',
 })
 export class MemberDetailed implements OnInit {
-  private memberService = inject(MemberService);
+  protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member = signal<Member | undefined>(undefined);
+
   protected title = signal<string | undefined>('Profile');
+  private accountService = inject(AccountService);
+  protected isCurrentUser = computed(() => {
+    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id');
+  });
 
   ngOnInit(): void {
-    this.route.data.subscribe({
-      next: (data) => this.member.set(data['member']),
-    });
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe({
