ebe58a227d3ea63c2fa7ae36308a8e0e7989b3ad
ADitta <ADitta@users.noreply.github.com>
2025-12-22 13:33:39 +0000

    update member functionality and add claims principle extension method

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,8 +1,11 @@
 ﻿
+using API.DTO;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 
 namespace API.Controllers;
@@ -29,6 +32,30 @@ public class MembersController(IMemberRepository memberRepository) : BaseApiCont
     public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
     {
         return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+    {
+        var memberId = User.GetMemberId();
+
+        var member = await memberRepository.GetMemberForUpdate(memberId);
+
+        if (member == null) return BadRequest("Could not get member.");
+
+        member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+        member.Description = memberUpdateDto.Description ?? member.Description;
+        member.City = memberUpdateDto.City ?? member.City;
+        member.Country = memberUpdateDto.Country ?? member.Country;
+
+        member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+        memberRepository.Update(member); // optional
+
+        if (await memberRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed up to update member.");
+
     }
 
 }
